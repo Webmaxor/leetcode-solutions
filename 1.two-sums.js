f6ce1b88c1268b355db68f1bc34cdc6fa7f28a38
Webmaxor <webmaxor@gmail.com>
2020-01-16 11:06:31 +0700

    1. Two sums

---
+++ 1.two-sums.js
@@ -0,0 +1,28 @@
+/**
+ * 1. Two sums[EASY][Bruteforce][O(n)]
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+ */
+
+ /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    const comp = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (comp[nums[i]] >= 0) {
+            return [comp[nums[i]], i];
+        }
+
+        comp[target - nums[i]] = i;
+    }
+
+    return [];
+};
