58bb4d930f0b83d70040a13ba1fe1865d875d5ee
Sherzod Shermukhamedov <webmaxor@gmail.com>
2020-01-18 18:24:41 +0700

    31. Next permutation

---
+++ 31.next-permutation.js
@@ -0,0 +1,47 @@
+/**
+ * 31. Next Permutation [MEDIUM]
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place and use only constant extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+  let i = nums.length - 2;
+
+  // Find first nums[i] > nums[i + 1] number
+  while (i >= 0 && nums[i + 1] <= nums[i]) {
+      i--;
+  }
+
+  if (i >= 0) {
+      let j = nums.length - 1;
+
+      while (j >= 0 && nums[j] <= nums[i]) {
+          j--;
+      }
+
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+  }
+
+  reverse(nums, i + 1);
+
+  return nums;
+};
+
+var reverse = function(nums, start) {
+  let i = start, j = nums.length - 1;
+
+  while (i < j) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      i++;
+      j--;
+  }
+}
