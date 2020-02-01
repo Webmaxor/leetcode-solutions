7acc0ddc5deb4ee2cf9ff8c05b30ad746bfd2543
Sherzod Shermukhamedov <webmaxor@gmail.com>
2020-02-01 12:48:31 +0700

    60. Permutation Sequence

---
+++ 60.permutation-sequence.js
@@ -0,0 +1,50 @@
+/**
+ * 60. Permutation Sequence [MEDIUM][Recursion]
+ * The set [1,2,3,...,n] contains a total of n! unique permutations.
+ * By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * Given n and k, return the kth permutation sequence.
+ *
+ * Note:
+ * Given n will be between 1 and 9 inclusive.
+ * Given k will be between 1 and n! inclusive.
+ *
+ * Example 1:
+ * Input: n = 3, k = 3
+ * Output: "213"
+ *
+ * Example 2:
+ * Input: n = 4, k = 9
+ * Output: "2314"
+ */
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function(n, k) {
+    const nums = [...Array(n)].map((item, index) => index + 1);
+    let res = [];
+
+    function find(curr, rest) {
+        if (!rest.length) return res.push(curr);
+        if (res.length >= k) return;
+
+        for (let i = 0; i < rest.length; i++) {
+            find(
+                [...curr, rest[i]],
+                [...rest.slice(0, i), ...rest.slice(i + 1)]
+            );
+        }
+    }
+
+    find([], nums);
+
+    return res[k - 1] ? res[k - 1].join("") : "";
+};
