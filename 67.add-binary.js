2237f0da21eae167c34f553f20cfc8df7202b7b3
Webmaxor <webmaxor@gmail.com>
2020-02-06 12:45:01 +0700

    67. Add binary

---
+++ 67.add-binary.js
@@ -0,0 +1,26 @@
+/**
+ * 67. Add Binary [EASY][String]
+ * Given two binary strings, return their sum (also a binary string).
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ *
+ * Example 1:
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ *
+ * Example 2:
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ */
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  const aBin = `0b${a}`;
+  const bBin = `0b${b}`;
+  const sum = BigInt(aBin) + BigInt(bBin);
+
+  return sum.toString(2);
+};
