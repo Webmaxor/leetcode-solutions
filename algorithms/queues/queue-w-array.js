e009a8cb6cf41c49ddd8515967334ce498cbc8aa
Sherzod Shermukhamedov <webmaxor@gmail.com>
2020-05-25 21:22:35 +0700

    Queue class

---
+++ algorithms/queues/queue-w-array.js
@@ -0,0 +1,38 @@
+class Queue {
+  constructor() {
+    // Creates an empty queue
+    this.arr = []
+  }
+
+  enqueue(item) {
+    // Inserts a new string onto queue
+    return this.arr.push(item)
+  }
+
+  dequeue() {
+    // removes and returns the most recently added
+    return this.arr.shift()
+  }
+
+  isEmpty() {
+    // is the queue empty?
+    return this.arr.length === 0
+  }
+
+  show() {
+    return this.arr
+  }
+}
+
+module.exports = Queue
+
+/**
+ * Usage
+ */
+// const queue = new Queue()
+// queue.enqueue(1)
+// queue.enqueue(3)
+// queue.enqueue(8)
+// queue.dequeue()
+// queue.enqueue(5)
+// console.log(queue.show())
---
+++ algorithms/queues/queue-w-linked-list.js
@@ -0,0 +1,40 @@
+const LinkedList = require('../linked-list/linked-list')
+
+class Queue {
+  constructor() {
+    // Creates an empty queue
+    this.linkedList = new LinkedList()
+  }
+
+  enqueue(item) {
+    // Inserts a new string onto queue
+    this.linkedList.push(item)
+  }
+
+  dequeue() {
+    // removes and returns the most recently added
+    this.linkedList.shift()
+  }
+
+  isEmpty() {
+    // is the queue empty?
+    return this.linkedList.head == null
+  }
+
+  show() {
+    return this.linkedList.head
+  }
+}
+
+module.exports = Queue
+
+/**
+ * Usage
+ */
+// const queue = new Queue()
+// queue.enqueue(1)
+// queue.enqueue(3)
+// queue.enqueue(8)
+// queue.dequeue()
+// queue.enqueue(5)
+// console.log(queue.show())
