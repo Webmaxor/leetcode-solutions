5852ce52715f3b149bd9fe9840a989cea031d22c
Webmaxor <webmaxor@gmail.com>
2021-02-05 10:30:24 +0700

    Easy the code

--- problems/1-49/04.median-of-2-sorted-arrays-o-log-m-n.js
+++ problems/1-49/04.median-of-2-sorted-arrays-o-log-m-n.js
@@ -21,30 +21,39 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-  if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1)
-  let x = nums1.length
-  let y = nums2.length
+const findMedianSortedArrays = (nums1, nums2) => {
+  const [small, large] = nums1.length < nums2.length ? [nums1, nums2] : [nums2, nums1];
+  const [x, y] = [small.length, large.length];
+
   let low = 0, high = x
+
   while(low <= high) {
-      const partitionX = (high + low) >> 1
-      const partitionY = ((x + y + 1) >> 1) - partitionX
+    const partitionX = Math.floor((high + low) / 2)
+    const partitionY = Math.floor((x + y + 1) / 2) - partitionX
 
-      const maxX = partitionX == 0 ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1]
-      const maxY = partitionY == 0 ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1]
+    const maxX = partitionX == 0 ? Number.NEGATIVE_INFINITY : small[partitionX - 1]
+    const maxY = partitionY == 0 ? Number.NEGATIVE_INFINITY : large[partitionY - 1]
 
-      const minX = partitionX == nums1.length ? Number.POSITIVE_INFINITY : nums1[partitionX]
-      const minY = partitionY == nums2.length ? Number.POSITIVE_INFINITY : nums2[partitionY ]
+    const minX = partitionX == x ? Number.POSITIVE_INFINITY : small[partitionX]
+    const minY = partitionY == y ? Number.POSITIVE_INFINITY : large[partitionY]
 
-      if(maxX <= minY && maxY <= minX) {
-          const lowMax = Math.max(maxX, maxY)
-          if( (x + y) % 2 == 1)
-              return lowMax
-          return (lowMax + Math.min(minX, minY)) / 2
-      } else if(maxX < minY) {
-          low = partitionX + 1
-      } else
-          high = partitionX - 1
+    if (maxX <= minY && maxY <= minX) {
+      const lowMax = Math.max(maxX, maxY)
+
+      if((x + y) % 2 == 1) {
+        return lowMax
+      }
+
+      return (lowMax + Math.min(minX, minY)) / 2
+    } else if (maxX < minY) {
+      low = partitionX + 1
+    } else {
+      high = partitionX - 1
+    }
   }
+}
 
-};
+/**
+ * USAGE
+ */
+console.log(findMedianSortedArrays([4, 20, 32, 50, 55, 61], [1, 15, 22, 30, 70]))
